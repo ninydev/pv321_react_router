a71e46e971218e74b9fa4948b90ca319a8dcd022
Oleksandr Nykytin <oleksandr.nykytin@tech.lab325.com>
2024-10-10 21:17:36 +0300

    lazy load

--- base_router/src/App.js
+++ base_router/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component, lazy, Suspense} from 'react';
 import './App.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,13 +6,15 @@ import 'react-toastify/dist/ReactToastify.css';
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
 import ContactsPage from "./pages/ContactsPage";
 import MainLayout from "./layouts/MainLayout";
 import NotFoundPage from "./pages/NotFoundPage";
 import CarsPage from "./pages/CarsPage";
 import CarModelPage from "./pages/CarModelPage";
 
+
+// Лениво загружаем AboutPage
+const AboutPage = lazy(() => import('./pages/AboutPage'));
 
 class App extends Component {
   render() {
@@ -23,7 +25,14 @@ class App extends Component {
         <Routes>
             <Route path="/" element={<MainLayout />}>
                 <Route path="" element={<HomePage />} />
-                <Route path="about" element={<AboutPage />} />
+
+                {/* Лениво загружаемый AboutPage */}
+                <Route path="about" element={
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <AboutPage />
+                    </Suspense>
+                } />
+
                 <Route path="cars" element={<CarsPage />} />
                     {/*<Route path="cars/:car_slug" element={<CarModelPage />} />*/}
                     <Route path="cars/:seo_text/:car_slug" element={<CarModelPage />} />
